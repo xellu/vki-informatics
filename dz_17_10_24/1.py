07dae3cb8eb53878cd223cc5ae6678b677b4a254
Xellu <83588955+xellu@users.noreply.github.com>
2024-10-23 09:25:27 +0700

    Delete dz_17_10_24 directory

--- dz_17_10_24/1.py
+++
@@ -1,3 +0,0 @@
-a = [i for i in range(1, 100) if i % 5 == 3]
-print(a)
-print(list(reversed(a)))
--- dz_17_10_24/3.py
+++
@@ -1,9 +0,0 @@
-def shift(array):
-    array.append(array.pop(0))
-    return array
-
-
-nums = [i+1 for i in range(15)]
-for _ in range(len(nums)):
-    print(nums)
-    nums = shift(nums)
